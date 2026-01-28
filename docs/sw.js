0c124b5891b94aa2d437a07406d6557f7db371e7
lenny <imhule@qq.com>
2026-01-28 20:44:54 +0800

    H5改造PWA

---
+++ docs/sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'easy-pwd-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './static/index.ed4a2d2b.css',
+  './static/js/chunk-vendors.1f652273.js',
+  './static/js/index.51216527.js',
+  './static/logo.png',
+  './static/images/tab/home.png',
+  './static/images/tab/home-active.png',
+  './static/images/tab/about.png',
+  './static/images/tab/about-active.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => (key === CACHE_NAME ? null : caches.delete(key))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const requestURL = new URL(event.request.url);
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      caches.match('./index.html').then((cached) => cached || fetch(event.request))
+    );
+    return;
+  }
+
+  if (requestURL.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(event.request).then((cached) =>
+        cached || fetch(event.request).then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+      )
+    );
+  }
+});
